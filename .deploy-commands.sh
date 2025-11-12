7c5397eac42489dd7aa1de0cfa7f426a143aeb88
sandeshmindsfire <sandeshparjanya@mindsfire.com>
2025-11-12 20:40:07 +0530

    Help function is added and Updated the code

--- .deploy-commands.sh
+++ .deploy-commands.sh
@@ -1,6 +1,12 @@
+# deploy to gcloud with the local source code
 gcloud run deploy hunt-whatsappapi \
   --source . \
   --project hunt-whatsappapi \
   --region asia-south1 \
   --allow-unauthenticated \
   --env-vars-file=.env
+
+# to stream the logs
+gcloud alpha run services logs tail hunt-whatsappapi \
+  --region asia-south1 \
+  --project hunt-whatsappapi
